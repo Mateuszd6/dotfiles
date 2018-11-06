c7f8b77164e5a4f26981d0362d6079ffed15ebba
Mateusz Dudzinski <mateuszpd7@gmail.com>
2018-11-06 14:32:28 +0100

    I3 configuration changes.

--- i3/.i3/package-checker.sh
+++ i3/.i3/package-checker.sh
@@ -1,18 +1,17 @@
-
 # NOTE: There is a hook in pacman config folder that sends
 #       singal to i3blocks to that after each update/installation/etc
 #       this is updated.
 
 full_text="";
 
-full=`yaourt -Quk | wc -l`
+full=`yay -Quk | wc -l`
 repo=`pacman -Quk | wc -l`
 aur=$((full - repo))
 
 if [ $repo -ne 0 ]; then
-    full_text="$full_text$repo(REPO)"
+    full_text="$full_text$repo(Repo)"
     if [ $aur -ne 0 ]; then
-	full_text="$full_text+$aur(AUR)"
+        full_text="$full_text+$aur(Aur)"
     fi
 elif [ $aur -ne 0 ]; then
     full_text="$full_text$aur(AUR)"
@@ -24,4 +23,3 @@ echo "$full_text"
 if [ $repo -ne 0 ]; then
     echo "#A8FF00"
 fi
-
